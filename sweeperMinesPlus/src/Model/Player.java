9253308d1f024c358f63438cbb05777354077ca5
ZRemy <116194611+ZRemy@users.noreply.github.com>
2022-11-27 19:01:53 -0500

    [DEV-2.5] Added java docs

--- sweeperMinesPlus/src/Model/Player.java
+++ sweeperMinesPlus/src/Model/Player.java
@@ -1,16 +1,26 @@
 package Model;
 
+/** Represents the MineSweeper player.
+ *
+ */
 public class Player {
     protected int score;
     protected String name;
     protected int lives;
 
+    /**
+     * Constructor for a MineSweeper player.
+     */
     public  Player(int s, String n, int l){
         score = s;
         name = n;
         lives = l;
     }
 
+    /**
+     * Have the player execute a move and apply the grid item to a player
+     * @param item grid item uncovered
+     */
     protected void move(GridItem item){
         if (item instanceof Bomb || item instanceof BonusLife){
             lives += item.applygridItem();
