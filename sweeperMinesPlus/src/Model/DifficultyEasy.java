b531039e09a0f147ec021943efcb023c485edf3c
Steven Hua <steven_hua@outlook.com>
2022-12-01 13:08:56 -0500

    [DEV-1.7] Completed AdversarialAI, Began Implementing AIMove for the Strategy Pattern

--- sweeperMinesPlus/src/Model/DifficultyEasy.java
+++ sweeperMinesPlus/src/Model/DifficultyEasy.java
@@ -3,9 +3,11 @@ package Model;
 public class DifficultyEasy implements AIDifficulty {
 
     private double bombChance;
+    private double bonusLifeChance;
 
     public DifficultyEasy() {
         this.bombChance = .2;
+        this.bonusLifeChance = .05;
     }
     @Override
     public void AIMove(GridItem model) {
--- sweeperMinesPlus/src/Model/DifficultyHard.java
+++ sweeperMinesPlus/src/Model/DifficultyHard.java
@@ -3,12 +3,15 @@ package Model;
 public class DifficultyHard implements AIDifficulty{
 
     private double bombChance;
+    private double bonusLifeChance;
 
     public DifficultyHard() {
         this.bombChance = .05;
+        this.bonusLifeChance = .15;
     }
 
     @Override
     public void AIMove(GridItem model) {
+
     }
 }
