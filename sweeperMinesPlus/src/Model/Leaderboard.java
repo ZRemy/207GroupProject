1256f0f47ba51b79840cf0bf47b15f0a860108d4
Marc D'Amico <marc.damico@mail.utoronto.ca>
2022-12-01 20:18:30 -0500

    [DEV-1.5] resolving conflict with pull request

--- sweeperMinesPlus/src/Model/Leaderboard.java
+++ sweeperMinesPlus/src/Model/Leaderboard.java
@@ -10,16 +10,10 @@ import java.util.HashMap;
 
 public final class Leaderboard {
 
-    private String playerName;
-    private int age;
-    private int number;
     private static final Leaderboard leaderboard = new Leaderboard();
+    public HashMap<String, Integer> playerScores = new HashMap<>();
 
-    public Leaderboard() {
-
-    }
-
-
+    private Leaderboard(){}
 
     /**
      * Getter method.
@@ -28,26 +22,5 @@ public final class Leaderboard {
      */
     public static Leaderboard getInstance(){
         return leaderboard;
-    }
-
-    public String getName() {
-        return playerName;
-    }
-
-    public void setName(String name) {
-        this.playerName = name;
-    }
-
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
     }
 }
