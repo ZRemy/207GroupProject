a074c2557474e5eaeb152fd0826ec90ae8ed792d
Marc D'Amico <marc.damico@mail.utoronto.ca>
2022-12-01 19:19:35 -0500

    [DEV-1.5] Used Scene Builder in colab with fxml for leaderboard scene

--- sweeperMinesPlus/src/Model/Leaderboard.java
+++ sweeperMinesPlus/src/Model/Leaderboard.java
@@ -2,15 +2,52 @@ package Model;
 
 import java.util.HashMap;
 
+/**
+ * Leaderboard model for minesweeper game.
+ */
+
+//TODO: put this back into the singleton pattern
+
 public final class Leaderboard {
 
+    private String playerName;
+    private int age;
+    private int number;
     private static final Leaderboard leaderboard = new Leaderboard();
-    public HashMap<String, Integer> playerScores = new HashMap<>();
 
-    private Leaderboard(){}
+    public Leaderboard() {
 
+    }
+
+
+
+    /**
+     * Getter method.
+     *
+     * @return the leaderboard singleton.
+     */
     public static Leaderboard getInstance(){
         return leaderboard;
     }
 
+    public String getName() {
+        return playerName;
+    }
+
+    public void setName(String name) {
+        this.playerName = name;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
