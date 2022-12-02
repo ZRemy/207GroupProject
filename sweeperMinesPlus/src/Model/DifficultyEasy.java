288d2ecc27c7c981ef275ce6433814f9ee62384b
ZRemy <116194611+ZRemy@users.noreply.github.com>
2022-12-02 10:15:28 -0500

    Revert "Merge pull request #18 from ZRemy/feature/1.7-AIDiffiuclty"
    This reverts commit 97d00abc273c65d1441697616c542ee73d9b028e, reversing
    changes made to 2964aee1edb4085156addaab356da7163deee977.

--- sweeperMinesPlus/src/Model/DifficultyEasy.java
+++ sweeperMinesPlus/src/Model/DifficultyEasy.java
@@ -3,11 +3,9 @@ package Model;
 public class DifficultyEasy implements AIDifficulty {
 
     private double bombChance;
-    private double bonusLifeChance;
 
     public DifficultyEasy() {
         this.bombChance = .2;
-        this.bonusLifeChance = .05;
     }
     @Override
     public void AIMove(GridItem model) {
--- sweeperMinesPlus/src/Model/DifficultyMedium.java
+++ sweeperMinesPlus/src/Model/DifficultyMedium.java
@@ -3,11 +3,9 @@ package Model;
 public class DifficultyMedium implements AIDifficulty{
 
     private double bombChance;
-    private double bonusLifeChance;
 
     public DifficultyMedium() {
         this.bombChance = .1;
-        this.bonusLifeChance = .1;
     }
 
     @Override
