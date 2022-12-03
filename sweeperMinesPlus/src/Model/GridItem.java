3ced118e0b69ab3569d7511d9c5dcdf6ab485c3e
ZRemy <116194611+ZRemy@users.noreply.github.com>
2022-12-03 16:22:52 -0500

    [DEV-1.3] Created Sanity tests for the Factory and for the GridItem apply methods. Added Java docs and remove the access modifier from applygridItem in the interface.

--- sweeperMinesPlus/src/Model/GridItem.java
+++ sweeperMinesPlus/src/Model/GridItem.java
@@ -1,7 +1,15 @@
 package Model;
 
+/** A GridItem for a minesweeper game.
+ *
+ */
 public interface GridItem {
 
-    public int applygridItem();
+    /**
+     * Applies the current GridItem, either a Bomb, BonusLife, or Empty item to the game/player.
+     * @return The number of surrounding bombs if the GridItem is of type Empty, The number of lives to remove if the
+     * type of the GridItem is Bomb, or the number of lives to restore if the GridItem is of type BonusLife.
+     */
+    int applygridItem();
 
 }
---
+++ sweeperMinesPlus/src/Model/GridItemTests.java
@@ -0,0 +1,35 @@
+package Model;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class GridItemTests {
+    @Test
+    void testFactory() {
+        SweeperBoard board = new SweeperBoard(16, 16, 40, 9);
+        GridItem a = GriditemFactory.createGridItem("bomb", 0, board, 0, 0);
+        GridItem b = GriditemFactory.createGridItem("empty", 0, board, 1, 1);
+        GridItem c = GriditemFactory.createGridItem("bonuslife", 2, board, 0, 0);
+        assert(a instanceof Bomb);
+        assert(b instanceof Empty);
+        assert(c instanceof BonusLife);
+    }
+    @Test
+    void testGridItemApply() {
+        SweeperBoard board = new SweeperBoard(16, 16, 40, 9);
+        GridItem a = GriditemFactory.createGridItem("bomb", 0, board, 0, 0);
+        GridItem b = GriditemFactory.createGridItem("empty", 0, board, 1, 1);
+        GridItem c = GriditemFactory.createGridItem("bonuslife", 2, board, 0, 0);
+        assertEquals(-1, a.applygridItem());
+        assertEquals(2, c.applygridItem());
+        assert(b.applygridItem() >= 0);
+    }
+
+
+}
+
